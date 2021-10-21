b9ab27b3cabfa5f9963023b42a1783ee824f0547
sunjiaqing <sundada214@outlook.com>
2021-10-21 14:23:36 +0800

    时间与字符串转换优化

--- src/main/java/com/github/shy526/date/DateFormatUtils.java
+++ src/main/java/com/github/shy526/date/DateFormatUtils.java
@@ -15,19 +15,45 @@ import java.util.concurrent.TimeUnit;
  * @author shy526
  */
 public final class DateFormatUtils {
-    private final static String DEFAULT_FORMAT = "yyyy-MM-dd HH:mm:ss";
-    private final static String DATE_FORMAT = "yyyy-MM-dd";
-    private final static String TIME_FORMAT = "HH:mm:ss";
-    private final static SimpleDateFormat DATE_DEFAULT_FORMAT = new SimpleDateFormat(DEFAULT_FORMAT);
-    private final static SimpleDateFormat DATE_DATE_FORMAT = new SimpleDateFormat(DATE_FORMAT);
-    private final static SimpleDateFormat DATE_TIME_FORMAT = new SimpleDateFormat(TIME_FORMAT);
-    private final static DateTimeFormatter LOCAL_DATE_DEFAULT_FORMAT = DateTimeFormatter.ofPattern(DEFAULT_FORMAT);
-    private final static DateTimeFormatter LOCAL_DATE_DATE_FORMAT = DateTimeFormatter.ofPattern(DATE_FORMAT);
-    private final static DateTimeFormatter LOCAL_DATE_TIME_FORMAT = DateTimeFormatter.ofPattern(TIME_FORMAT);
-    private final static int QUEUE_SIZE = 20;
+    /**
+     * 通用格式化格式
+     */
+    public final static String DEFAULT_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public final static String DATE_FORMAT = "yyyy-MM-dd";
+    public final static String TIME_FORMAT = "HH:mm:ss";
+
+    /**
+     * 提供时间与字符串转换的队列
+     */
+    private final static int QUEUE_SIZE = Runtime.getRuntime().availableProcessors() + 1;
     private final static BlockingQueue<SimpleDateFormat> DATE_DEFAULT_FORMAT_QUEUE = new ArrayBlockingQueue<>(QUEUE_SIZE);
     private final static BlockingQueue<SimpleDateFormat> DATE_DATE_FORMAT_QUEUE = new ArrayBlockingQueue<>(QUEUE_SIZE);
     private final static BlockingQueue<SimpleDateFormat> DATE_TIME_FORMAT_QUEUE = new ArrayBlockingQueue<>(QUEUE_SIZE);
+
+    /**
+     * 新时间格式转化
+     */
+    private final static DateTimeFormatter LOCAL_DATE_DEFAULT_FORMAT = DateTimeFormatter.ofPattern(DEFAULT_FORMAT);
+    private final static DateTimeFormatter LOCAL_DATE_DATE_FORMAT = DateTimeFormatter.ofPattern(DATE_FORMAT);
+    private final static DateTimeFormatter LOCAL_TIME_FORMAT = DateTimeFormatter.ofPattern(TIME_FORMAT);
+
+    private final static AbstractProcessor<Date, String> FORMAT_PROCESSOR = new AbstractProcessor<Date, String>() {
+        @Override
+        protected String handle(Date obj, SimpleDateFormat sdf) {
+            return sdf.format(obj);
+        }
+    };
+    private final static AbstractProcessor<String, Date> PARSE_PROCESSOR = new AbstractProcessor<String, Date>() {
+        @Override
+        protected Date handle(String obj, SimpleDateFormat sdf) {
+            try {
+                return sdf.parse(obj);
+            } catch (ParseException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    };
 
     static {
         for (int i = 0; i < QUEUE_SIZE; i++) {
@@ -37,53 +63,6 @@ public final class DateFormatUtils {
         }
     }
 
-    /**
-     * 获取一个时间格式化的str
-     *
-     * @param format format
-     * @param date   date
-     * @return String
-     */
-    public static String getFormatStr(String format, Date date) {
-        SimpleDateFormat result = null;
-        BlockingQueue<SimpleDateFormat> blockingQueue = null;
-        if (DEFAULT_FORMAT.equals(format)) {
-            blockingQueue = DATE_DEFAULT_FORMAT_QUEUE;
-        } else if (DATE_FORMAT.equals(format)) {
-            blockingQueue = DATE_DATE_FORMAT_QUEUE;
-        } else if (TIME_FORMAT.equals(format)) {
-            blockingQueue = DATE_TIME_FORMAT_QUEUE;
-        } else {
-            result = new SimpleDateFormat(format);
-        }
-
-        String resultFormat = null;
-        if (blockingQueue != null) {
-            result = formatQueuePoll(blockingQueue, format);
-            resultFormat = result.format(date);
-            blockingQueue.add(result);
-        } else {
-            resultFormat = result.format(date);
-        }
-        return resultFormat;
-    }
-
-    /**
-     * 从指定队列中获取 SimpleDateFormat 当队列用尽 时创建一个返回
-     *
-     * @param blockingQueue blockingQueue
-     * @param format        format
-     * @return SimpleDateFormat
-     */
-    public static SimpleDateFormat formatQueuePoll(BlockingQueue<SimpleDateFormat> blockingQueue, String format) {
-        SimpleDateFormat result = null;
-        try {
-            result = blockingQueue.poll(1, TimeUnit.SECONDS);
-        } catch (Exception e) {
-            result = new SimpleDateFormat(format);
-        }
-        return result;
-    }
 
     /**
      * 转换为默认时间
@@ -92,7 +71,7 @@ public final class DateFormatUtils {
      * @return String
      */
     public static String defaultFormat(Date date) {
-        return getFormatStr(DEFAULT_FORMAT, date);
+        return format(date, DEFAULT_FORMAT);
 
     }
 
@@ -103,11 +82,7 @@ public final class DateFormatUtils {
      * @return Date
      */
     public static Date defaultParse(String dateStr) {
-        try {
-            return DATE_DEFAULT_FORMAT.parse(dateStr);
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
+        return parse(dateStr, DEFAULT_FORMAT);
     }
 
     /**
@@ -117,7 +92,7 @@ public final class DateFormatUtils {
      * @return String
      */
     public static String dateFormat(Date date) {
-        return getFormatStr(DATE_FORMAT, date);
+        return format(date, DATE_FORMAT);
     }
 
     /**
@@ -127,11 +102,7 @@ public final class DateFormatUtils {
      * @return Date
      */
     public static Date dateParse(String dateStr) {
-        try {
-            return DATE_DATE_FORMAT.parse(dateStr);
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
+        return parse(dateStr, DATE_FORMAT);
     }
 
     /**
@@ -141,7 +112,7 @@ public final class DateFormatUtils {
      * @return String
      */
     public static String timeFormat(Date date) {
-        return getFormatStr(TIME_FORMAT, date);
+        return format(date, TIME_FORMAT);
     }
 
     /**
@@ -151,23 +122,18 @@ public final class DateFormatUtils {
      * @return Date
      */
     public static Date timeParse(String dateStr) {
-        try {
-            return DATE_TIME_FORMAT.parse(dateStr);
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
+        return parse(dateStr, TIME_FORMAT);
     }
 
     /**
-     * 自定义foramt
+     * 自定义format
      *
      * @param date   时间
      * @param format 格式
      * @return String
      */
     public static String format(Date date, String format) {
-        SimpleDateFormat sdf = new SimpleDateFormat(format);
-        return sdf.format(date);
+        return FORMAT_PROCESSOR.run(date, format);
     }
 
     /**
@@ -178,12 +144,7 @@ public final class DateFormatUtils {
      * @return Date
      */
     public static Date parse(String dataStr, String format) {
-        SimpleDateFormat sdf = new SimpleDateFormat(format);
-        try {
-            return sdf.parse(dataStr);
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
+        return PARSE_PROCESSOR.run(dataStr, format);
     }
 
     /**
@@ -213,7 +174,7 @@ public final class DateFormatUtils {
      * @return String
      */
     public static String timeFormat(TemporalAccessor localDateTime) {
-        return LOCAL_DATE_TIME_FORMAT.format(localDateTime);
+        return LOCAL_TIME_FORMAT.format(localDateTime);
     }
 
     /**
@@ -226,5 +187,44 @@ public final class DateFormatUtils {
     public static String format(TemporalAccessor localDateTime, String format) {
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(format);
         return dateTimeFormatter.format(localDateTime);
+    }
+
+    static abstract class AbstractProcessor<J, T> {
+        public T run(J obj, String format) {
+            T result = null;
+            BlockingQueue<SimpleDateFormat> blockingQueue = null;
+            if (DEFAULT_FORMAT.equals(format)) {
+                blockingQueue = DATE_DEFAULT_FORMAT_QUEUE;
+            } else if (DATE_FORMAT.equals(format)) {
+                blockingQueue = DATE_DATE_FORMAT_QUEUE;
+            } else if (TIME_FORMAT.equals(format)) {
+                blockingQueue = DATE_TIME_FORMAT_QUEUE;
+            }
+            SimpleDateFormat sdf = null;
+            if (blockingQueue != null) {
+                try {
+                    sdf = blockingQueue.poll(100, TimeUnit.MILLISECONDS);
+                } catch (Exception e) {
+                    sdf = new SimpleDateFormat(format);
+                }
+                result = handle(obj, sdf);
+                blockingQueue.offer(sdf);
+
+            } else {
+                sdf = new SimpleDateFormat(format);
+                result = handle(obj, sdf);
+            }
+            return result;
+        }
+
+        /**
+         * 处理方法
+         *
+         * @param obj              obj
+         * @param sdf simpleDateFormat
+         * @return T
+         */
+        protected abstract T handle(J obj, SimpleDateFormat sdf);
+
     }
 }
--- src/test/java/com/github/shy526/date/DateFormatUtilsTest.java
+++ src/test/java/com/github/shy526/date/DateFormatUtilsTest.java
@@ -4,6 +4,8 @@ import com.github.shy526.thread.ThreadPoolUtils;
 import org.junit.Test;
 
 import java.util.Date;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.CountDownLatch;
 
 import static org.junit.Assert.*;
 
@@ -11,11 +13,21 @@ public class DateFormatUtilsTest {
 
     @Test
     public void defaultFormat() {
+        int max=100;
+        CountDownLatch countDownLatch = new CountDownLatch(max);
         Date date = new Date();
-        for (int i=0;i<=100;i++){
+        for (int i=0;i<=max;i++){
             new Thread(()->{
-                String stt = DateFormatUtils.defaultFormat(date);
+                String dateStr = DateFormatUtils.format(date,"yyyyMMdd HH:mm:ss");
+                String dateSt1= DateFormatUtils.defaultFormat(date);
+                System.out.println(dateStr);
+                countDownLatch.countDown();
             }).start();
+        }
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
