acd554c420ec93b17b58f756ac6679aa083e3fa7
sunjiaqing <sundada214@outlook.com>
2021-10-21 14:59:16 +0800

    常用字符串处理

---
+++ src/main/java/com/github/shy526/string/StringHandle.java
@@ -0,0 +1,31 @@
+package com.github.shy526.string;
+
+import java.util.regex.Pattern;
+
+/**
+ * 收集一些常见的字符串处理方式
+ * @author shy526
+ */
+public class StringHandle {
+
+    private static final Pattern PHONE_PATTERN = Pattern.compile("^((13[0-9])|(15[^4,\\D])|(17[0-9])|(18[0-9]))\\d{8}$");
+
+    private static final Pattern IP_PATTERN = Pattern.compile("\\b((?!\\d\\d\\d)\\d+|1\\d\\d|2[0-4]\\d|25[0-5])\\.((?!\\d\\d\\d)\\d+|1\\d\\d|2[0-4]\\d|25[0-5])\\.((?!\\d\\d\\d)\\d+|1\\d\\d|2[0-4]\\d|25[0-5])\\.((?!\\d\\d\\d)\\d+|1\\d\\d|2[0-4]\\d|25[0-5])\\b");
+
+    public static boolean ifPhone(String phone) {
+
+        return PHONE_PATTERN.matcher(phone).matches();
+    }
+
+    public static boolean ifIp(String ip) {
+        return IP_PATTERN.matcher(ip).matches();
+    }
+
+    public static String phoneHide(String phone) {
+        return phone.replaceAll("(\\d{3})\\d{4}(\\d{4})", "$1****$2");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ifPhone(null));
+    }
+}
